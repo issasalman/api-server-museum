455372ea4c269889359573a18fdbdefaa818f0c1
issasalman <issasalmanhu1996@gmail.com>
2021-09-21 05:32:10 +0300

    done

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-
 const PORT = process.env.PORT;
 
 const { getUser } = require("./controllers/museum.controller");
@@ -16,7 +15,6 @@ app.get("/", getUser);
 
 // const getIndex = require("./controllers/index.controller");
 // app.get("/", getIndex);
-
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
